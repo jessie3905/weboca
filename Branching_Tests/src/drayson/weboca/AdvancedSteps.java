eff0064b61c482bb0338901e353b4954a8c89d68
michael.drayson <michael.drayson@1ef41a26-3929-0410-9773-73ad8f2cc4c7>
2007-02-14 18:49:43 +0000

    Updated branching files

--- Branching_Tests/src/drayson/weboca/AdvancedSteps.java
+++ Branching_Tests/src/drayson/weboca/AdvancedSteps.java
@@ -12,15 +12,17 @@ public class AdvancedSteps extends WizardPanelProvider {
     /** Creates a new instance of CatLoversSteps */
     public AdvancedSteps() {
         super (
-            new String[] { "something", "one" }, 
-            new String[] { "Choose Something", "Choose one" });
+            new String[] { "a", "b" }, 
+            new String[] { "c", "d" });
     }
     
     protected JComponent createPanel(WizardController controller, String id, Map settings) {
         switch (indexOfStep(id)) {
             case 0 :
+                System.out.println("Case 0 in AdvancedSteps.java");
                 return new WelcomePanel (controller, settings);
             case 1 :
+                System.out.println("Case 1 in AdvancedSteps.java");
                 return new WelcomePanel (controller, settings);
             default :
                 throw new IllegalArgumentException (id);
--- Branching_Tests/src/drayson/weboca/NewWeBoCaWizard.java
+++ Branching_Tests/src/drayson/weboca/NewWeBoCaWizard.java
@@ -5,6 +5,7 @@ import org.netbeans.spi.wizard.WizardBranchController;
 import org.netbeans.spi.wizard.WizardPanelProvider;
 import drayson.weboca.gui.WelcomePanel;
 import javax.swing.SwingUtilities;
+import org.netbeans.api.wizard.WizardDisplayer;
 
 
 /**
@@ -23,11 +24,14 @@ public class NewWeBoCaWizard extends WizardBranchController {
         //value of step
         Object userType = collectedData.get(WelcomePanel.userType);
         
-        if (WelcomePanel.VALUE_SIMPLE.equals("simple")) {
+        if (WelcomePanel.VALUE_CURRENT == "simple") {
+            //System.out.println("VALUE_SIMPLE on Welcome Panel equals 'simple'");
             return getSimpleSteps();
-        } else if (WelcomePanel.VALUE_ADVANCED.equals("advanced")) {
+        } else if (WelcomePanel.VALUE_CURRENT == "advanced") {
+            //System.out.println("VALUE_SIMPLE on Welcome Panel equals 'advanced'");
             return getAdvancedSteps();
         } else {
+            //System.out.println("VALUE_SIMPLE on Welcome Panel is neither, null returned");
             return null;
         }
     }
@@ -49,13 +53,16 @@ public class NewWeBoCaWizard extends WizardBranchController {
     private SimpleSteps simpleSteps = null;
     private AdvancedSteps advancedSteps = null;
     
-    public static void main(String[] args) {
-        
-        SwingUtilities.invokeLater(new Runnable(){
-            public void run() {
-                new NewWeBoCaWizard();
-            }});
-        
-    }
+    
+    
+   public static void main(String[] args) {
+
+   SwingUtilities.invokeLater(new Runnable(){ public void run() {
+
+   WizardDisplayer.showWizard (new NewWeBoCaWizard().createWizard()) ;
+
+    }});
+
+}
     
 }
