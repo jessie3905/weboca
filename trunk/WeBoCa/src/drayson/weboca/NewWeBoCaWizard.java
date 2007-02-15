152a549bedba59f6942edfe2cc42d10549ef3570
michael.drayson <michael.drayson@1ef41a26-3929-0410-9773-73ad8f2cc4c7>
2007-02-15 12:38:08 +0000

    Native look and feel of the Swing UI manager has now been changed to look less ugly.

--- trunk/WeBoCa/src/drayson/weboca/NewWeBoCaWizard.java
+++ trunk/WeBoCa/src/drayson/weboca/NewWeBoCaWizard.java
@@ -6,6 +6,8 @@ import org.netbeans.spi.wizard.WizardPanelProvider;
 import drayson.weboca.gui.WelcomePanel;
 import javax.swing.SwingUtilities;
 import org.netbeans.api.wizard.WizardDisplayer;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 
 /**
@@ -16,22 +18,41 @@ import org.netbeans.api.wizard.WizardDisplayer;
 public class NewWeBoCaWizard extends WizardBranchController {
 
     NewWeBoCaWizard(  ) {
+        
         super( new InitialSteps(  ) );
+        
+        // Get the native look and feel class name
+        String nativeLF = UIManager.getSystemLookAndFeelClassName();
+        
+        // Motif is ugly. Use the cross-platform instead.
+        if (nativeLF.equals("com.sun.java.swing.plaf.motif.MotifLookAndFeel")) {
+            nativeLF = UIManager.getCrossPlatformLookAndFeelClassName();
+        }
+    
+        // Install the look and feel and catch any errors
+        try {
+            UIManager.setLookAndFeel(nativeLF);
+        } catch (InstantiationException e) {
+            System.err.println(e);
+        } catch (ClassNotFoundException e) {
+            System.err.println(e);
+        } catch (UnsupportedLookAndFeelException e) {
+            System.err.println(e);
+        } catch (IllegalAccessException e) {
+            System.err.println(e);
+        }
+        
     }
    
-    protected WizardPanelProvider getPanelProviderForStep(String step, Map collectedData) {
-        //There's only one branch point, so we don't need to test the
-        //value of step
+    protected WizardPanelProvider getPanelProviderForStep(String step, Map collectedData) 
+    {
         Object userType = collectedData.get(WelcomePanel.userType);
         
         if (WelcomePanel.VALUE_CURRENT == "simple") {
-            //System.out.println("VALUE_SIMPLE on Welcome Panel equals 'simple'");
             return getSimpleSteps();
         } else if (WelcomePanel.VALUE_CURRENT == "advanced") {
-            //System.out.println("VALUE_SIMPLE on Welcome Panel equals 'advanced'");
             return getAdvancedSteps();
         } else {
-            //System.out.println("VALUE_SIMPLE on Welcome Panel is neither, null returned");
             return null;
         }
     }
